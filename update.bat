bc3e1bd515e1a47c2c946bca51e42b84a0eb2244
furugiyamomotaro <mercari@analysis.local>
2026-03-22 16:06:21 +0900

    auto update

--- update.bat
+++ update.bat
@@ -1,14 +1,14 @@
 @echo off
 echo === Step1: データ更新中 ===
-cd I:\mercari_data
-python FULL_AUTO_LOCAL_Vol2.py
+cd /d I:\mercari_data
+python I:\mercari_data\FULL_AUTO_LOCAL_Vol2.py
 if errorlevel 1 goto error
 
 echo === Step2: outputコピー中 ===
 xcopy I:\mercari_data\output I:\mercari_github\output /E /I /Y
 
 echo === Step3: GitHubにpush中 ===
-cd I:\mercari_github
+cd /d I:\mercari_github
 git add -A
 git commit -m "auto update"
 git push
